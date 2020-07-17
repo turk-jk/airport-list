d8a68bfadbdb83cad952eb0470d40bd9591f4018
turk-jk <jacob.kazal@gmail.com>
2020-07-17 17:23:28 +0800

    Initial Commit

---
+++ airport list/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  airport list
+//
+//  Created by yacob jamal kazal on 17/7/20.
+//  Copyright © 2020 yacob jamal kazal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
